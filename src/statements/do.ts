74db9274302943c83409b54252299f08497793aa
dcodeIO <dcode@dcode.io>
2017-05-26 20:18:01 +0200

    Check for empty blocks in loops

--- src/statements/do.ts
+++ src/statements/do.ts
@@ -1,16 +1,18 @@
 import { Compiler } from "../compiler";
 import { intType } from "../types";
 import { binaryen } from "../wasm";
+import * as wasm from "../wasm";
 
 export function compileDo(compiler: Compiler, node: ts.DoStatement, onVariable: (node: ts.VariableDeclaration) => number): binaryen.Statement {
   const op = compiler.module;
+  const statement = compiler.compileStatement(node.statement, onVariable);
 
   compiler.enterBreakContext();
-  const label = compiler.currentBreakLabel;
 
+  const label = compiler.currentBreakLabel;
   const context = op.loop("break$" + label, op.block("continue$" + label, [
-    compiler.compileStatement(node.statement, onVariable),
-    op.break("break$" + label, op.i32.eqz(compiler.maybeConvertValue(node.expression, compiler.compileExpression(node.expression, intType), (<any>node.expression).wasmType, intType, true)))
+    statement || op.nop(),
+    op.break("break$" + label, op.i32.eqz(compiler.maybeConvertValue(node.expression, compiler.compileExpression(node.expression, intType), <wasm.Type>(<any>node.expression).wasmType, intType, true)))
   ]));
 
   compiler.leaveBreakContext();
--- src/statements/while.ts
+++ src/statements/while.ts
@@ -5,15 +5,17 @@ import * as wasm from "../wasm";
 
 export function compileWhile(compiler: Compiler, node: ts.WhileStatement, onVariable: (node: ts.VariableDeclaration) => number): binaryen.Statement {
   const op = compiler.module;
+  const statement = compiler.compileStatement(node.statement, onVariable);
 
   compiler.enterBreakContext();
-  const label = compiler.currentBreakLabel;
 
+  const label = compiler.currentBreakLabel;
   const context = op.loop("break$" + label, op.block("continue$" + label, [
     op.break("break$" + label, op.i32.eqz(compiler.maybeConvertValue(node.expression, compiler.compileExpression(node.expression, intType), <wasm.Type>(<any>node.expression).wasmType, intType, true))),
-    compiler.compileStatement(node.statement, onVariable)
+    statement || op.nop()
   ]));
 
   compiler.leaveBreakContext();
+
   return context;
 }
