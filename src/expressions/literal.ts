4107f3279d95c3b820846b1113c330e1c9630435
dcodeIO <dcode@dcode.io>
2017-06-15 14:25:45 +0200

    Do not rely on contextual type too much when compiling literals, fixes #47

--- src/expressions/literal.ts
+++ src/expressions/literal.ts
@@ -27,72 +27,67 @@ export function compileLiteral(compiler: Compiler, node: typescript.LiteralExpre
       return compiler.uintptrSize === 4 ? op.i32.const(0) : op.i64.const(0, 0);
 
   }
+
   typescript.setReflectedType(node, contextualType);
 
-  if (contextualType.isAnyFloat) {
+  let intValue: number;
+  let intRadix: number = 10;
 
+  if (/^(?:0|[1-9][0-9]*)$/.test(text))
+    intValue = parseInt(text, intRadix = 10);
+  else if (/^0[xX][0-9A-Fa-f]+$/.test(text))
+    intValue = parseInt(text = text.substring(2), intRadix = 16);
+  else {
     let floatValue: number;
-
     if (/^(?![eE])[0-9]*(?:\.[0-9]*)?(?:[eE][+-]?[0-9]+)?$/.test(text)) {
       floatValue = parseFloat(text);
       if (negate)
         floatValue = -floatValue;
+
+      if (contextualType === reflection.floatType)
+        return op.f32.const(floatValue);
+      else {
+        typescript.setReflectedType(node, reflection.doubleType);
+        return op.f64.const(floatValue);
+      }
     } else {
-      floatValue = 0;
-      compiler.error(node, "Illegal float literal", text);
-    }
-
-    return contextualType === reflection.floatType
-      ? op.f32.const(floatValue)
-      : op.f64.const(floatValue);
-
-  } else if (contextualType.isAnyInteger) {
-
-    let intValue: number;
-    let intRadix: number;
-
-    if (/^(?:0|[1-9][0-9]*)$/.test(text)) {
-      intValue = parseInt(text, intRadix = 10);
-    } else if (/^0[xX][0-9A-Fa-f]+$/.test(text)) {
-      intValue = parseInt(text = text.substring(2), intRadix = 16);
-    } else {
-      intValue = 0; intRadix = 10; text = "0";
-      compiler.error(node, "Illegal integer literal", text);
-    }
-
-    if (negate)
-      intValue = -intValue;
-
-    switch (contextualType) {
-
-      case reflection.sbyteType:
-      case reflection.shortType:
-        return op.i32.const(((intValue >>> 0) << <number>contextualType.shift32) >> <number>contextualType.shift32);
-
-      case reflection.byteType:
-      case reflection.ushortType:
-        return op.i32.const(intValue & <number>contextualType.mask32);
-
-      case reflection.intType:
-      case reflection.uintType:
-      case reflection.uintptrType32:
-        return op.i32.const(intValue);
-
-      case reflection.boolType:
-        return op.i32.const(intValue ? 1 : 0);
-
-      case reflection.longType:
-      case reflection.ulongType:
-      case reflection.uintptrType64:
-        let long = Long.fromString(text, !contextualType.isSigned, intRadix);
-        if (negate)
-          long = long.negate();
-        return op.i64.const(long.low, long.high);
-
+      // TODO: strings
+      compiler.error(node, "Unsupported literal", "'" + text + "' in " + reflection.TypeKind[contextualType.kind] + " context\n" + (new Error()).stack);
+      return op.unreachable();
     }
   }
-  compiler.error(node, "Unsupported literal", "'" + text + "' in " + reflection.TypeKind[contextualType.kind] + " context\n" + (new Error()).stack);
-  return op.unreachable();
+
+  if (negate)
+    intValue = -intValue;
+
+  switch (contextualType) {
+
+    case reflection.sbyteType:
+    case reflection.shortType:
+      return op.i32.const(((intValue >>> 0) << <number>contextualType.shift32) >> <number>contextualType.shift32);
+
+    case reflection.byteType:
+    case reflection.ushortType:
+      return op.i32.const(intValue & <number>contextualType.mask32);
+
+    default:
+      typescript.setReflectedType(node, reflection.intType);
+    case reflection.intType:
+    case reflection.uintType:
+    case reflection.uintptrType32:
+      return op.i32.const(intValue);
+
+    case reflection.boolType:
+      return op.i32.const(intValue ? 1 : 0);
+
+    case reflection.longType:
+    case reflection.ulongType:
+    case reflection.uintptrType64:
+      let long = Long.fromString(text, !contextualType.isSigned, intRadix);
+      if (negate)
+        long = long.negate();
+      return op.i64.const(long.low, long.high);
+  }
 }
 
 export { compileLiteral as default };
